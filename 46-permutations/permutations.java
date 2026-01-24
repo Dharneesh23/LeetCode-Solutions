6e18f99e8eaebac35d63dee7b1dbe7fb03e236e9
Dharneesh S <ddharnish42@gmail.com>
2026-01-24 21:08:45 +0530

    Time: 1 ms (87.20%) | Memory: 45.8 MB (15.73%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, new ArrayList<>(), used, result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> current,
+                           boolean[] used, List<List<Integer>> result) {
+
+        // Base case
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        // Try all numbers
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            // Choose
+            used[i] = true;
+            current.add(nums[i]);
+
+            // Explore
+            backtrack(nums, current, used, result);
+
+            // Un-choose (backtrack)
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
