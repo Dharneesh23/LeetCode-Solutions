d004112ac4ae2de9ea8af8f9141afbf0c9f0e6b1
Dharneesh S <ddharnish42@gmail.com>
2026-04-26 19:05:33 +0530

    Time: 17 ms (87.27%) | Memory: 138.2 MB (66.70%) - LeetSync

---
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -0,0 +1,46 @@
+class Solution {
+    int m, n;
+    boolean[][] visited;
+    int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+    
+    public boolean containsCycle(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    if (dfs(grid, i, j, -1, -1)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] grid, int r, int c, int pr, int pc) {
+        visited[r][c] = true;
+
+        for (int[] d : dirs) {
+            int nr = r + d[0];
+            int nc = c + d[1];
+
+            if (nr < 0 || nc < 0 || nr >= m || nc >= n) continue;
+
+            // must have same character
+            if (grid[nr][nc] != grid[r][c]) continue;
+
+            // skip parent
+            if (nr == pr && nc == pc) continue;
+
+            // if already visited → cycle
+            if (visited[nr][nc]) return true;
+
+            if (dfs(grid, nr, nc, r, c)) return true;
+        }
+
+        return false;
+    }
+}
